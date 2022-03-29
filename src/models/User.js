a38ab070b5e9acfd51339218b8457c060b759585
Audry Wolters <audrywolters@gmail.com>
2022-03-29 12:46:59 -0500

    created signin route and also hash/salt password

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
 	email: {
@@ -11,5 +12,64 @@ const userSchema = new mongoose.Schema({
 		required: true
 	}
 })
+
+// pre-save hook for hashing/salting a password
+// this will run before we try to save the user to DB
+// using old keyword function() so we can grab 'this'. a.k.a. the user.
+userSchema.pre('save', function (next) {
+	// this/user is coming from DB
+	const user = this
+	// if user has same password - ignore this code
+	if (!user.isModified('password')) {
+		return next()
+	}
+
+	// if a new password generate a new salt
+	// 10 means complexity (length probably)
+	bcrypt.genSalt(10, (err, salt) => {
+		if (err) {
+			return next(err)
+		}
+
+		// hash and salt the password and callsbacks an error
+		bcrypt.hash(user.password, salt, (err, hash) => {
+			if (err) {
+				return next(err)
+			}
+
+			// go ahead and salt/hash the password (change it to crypted format)
+			user.password = hash
+
+			next()
+		})
+	})
+})
+
+// now we are checking existing passwords
+// using old keyword function() so we can grab 'this'. a.k.a. the user.
+userSchema.methods.comparePassword = function (candidatePassword) {
+	// this/user is coming from DB
+	const user = this
+
+	// use a promise because bycrypt isn't into async/await
+	return new Promise((resolve, reject) => {
+		// get bycrypt to hash/salt sent password
+		// and compare to what exists in DB
+		bcrypt.compare(candidatePassword, user.password, (err, isMatch) => {
+			// something went wrong
+			if (err) {
+				return reject(err)
+			}
+
+			// no match!
+			if (!isMatch) {
+				return reject(false)
+			}
+
+			// huzzah!
+			resolve(true)
+		})
+	})
+}
 
 mongoose.model('User', userSchema)
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,8 +7,8 @@ const User = mongoose.model('User')
 
 const router = express.Router()
 
+// sign up a new user
 router.post('/signup', async (req, res) => {
-	
 	// get the items out of the request
 	const { email, password } = req.body
 	const user = new User({ email, password })
@@ -16,7 +16,7 @@ router.post('/signup', async (req, res) => {
 	try {
 		// actually save the thing
 		await user.save()
-	
+
 		// make the JWT
 		// userId is the payload
 		// but the second argument is what makes it safer
@@ -28,12 +28,43 @@ router.post('/signup', async (req, res) => {
 		// can condense to just ({ token })
 		// but leaving for learninging
 		res.send({ token: token })
-
 	} catch (err) {
 		// 422 is user sent invalid data
 		// AUDRY - is it ok to send 422? what if it's a different error?
 		// err.message is not the best for a user but we're using it to debug for now
 		return res.status(422).send(err.message)
+	}
+})
+
+// sign in existsing user
+router.post('/signin', async (req, res) => {
+	// pull data out of request
+	const { email, password } = req.body
+
+	if (!email || !password) {
+		// 422 is user sent invalid data
+		return res.status(422).send({ error: 'Invalid password or maybe email.' })
+	}
+
+	// try to get existing user from DB
+	const user = await User.findOne({ email: email })
+	if (!user) {
+		// 404 is not found
+		return res.status(404).send({ error: 'Email not found.' })
+	}
+
+	// how are we knowing this bcrypt will get this?
+	// i guess it's part of User and mongoose
+	try {
+		await user.comparePassword(password)
+		// why are we generating a new token?
+		// cuz different path - different task
+		const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+		res.send({ token: token })
+	} catch (err) {
+		// 422 something is wrong with the data
+		// 404 is not found
+		return res.status(422).send({ error: 'Invalid password or maybe email.' })
 	}
 })
 
